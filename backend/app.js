7602af0115bed5bbccafb7826c2fb2e6e82d0e52
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-02-05 20:59:46 +0100

    Skapat mongodb + div labb

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const mongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoClient.connect('mongodb://127.0.0.1:27017')
+.then(client => {
+    console.log("Databasen är uppkopplad");
+
+    const db = client.db("per-rosen");
+    app.locals.db = db;
+})
+
+app.get('/donuts', (req, res) => {
+    //res.json({"what": "donuts"})
+    req.app.locals.db.collection("donuts").find().toArray()
+    .then(result => {
+        console.log("saved donut", res);
+        res.json(result)
+    })
+});
+
+//Fusk istället för postman:
+app.get('/add', (req, res) => {
+    let newDonut = {"name": "Choklad"}
+
+    req.app.locals.db.collection("donuts").insertOne(newDonut)
+    .then(result => {
+        console.log("saved donut", res);
+        res.json(result)
+    })
+    .catch(err => console.log('err', err))
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
