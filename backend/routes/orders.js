f447bb06ea7f55274a3e4c1e2bcedce4b7cf29c2
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-02-08 19:35:59 +0100

    Påbörjat posta ny order

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+/* POST new orders */
+router.post('/add', function(req, res, next) {
+
+  //Skapa order
+  req.app.locals.db.collection('orders').insertOne(req.body)
+
+  .then(results => {
+  res.send(req.body.products);
+  });
+
+  //Products
+  req.app.locals.db.collection('products').find().toArray()
+  
+  .then(results => {
+
+    
+    
+    for (product in results) {
+      console.log(results[product].lager)
+    }
+    console.log(req.body.products);
+
+  })
+
+});
+
 /* GET orders listing. */
 router.get('/', function(req, res, next) {
   res.send('Orders router');
 });
-
 module.exports = router;
